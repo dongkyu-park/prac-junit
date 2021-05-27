7536cb9fed4b5c77752c9c1b9fcb3d7cacea7048
dongkyu Park <dongkyu.parkk@gmail.com>
2021-05-27 16:53:15 +0900

    feat: Junit5 테스트 반복하기

--- src/test/java/me/kyu/pracjunit/StudyTest.java
+++ src/test/java/me/kyu/pracjunit/StudyTest.java
@@ -1,6 +1,16 @@
 package me.kyu.pracjunit;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -11,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudyTest {
 
     @Test
-    @DisplayName("스터디 만들기 😀")
+    @DisplayName("스터디 만들기 assertAll😀")
     void create_new_study() {
 
         Study study = new Study(10);
@@ -34,7 +44,7 @@ class StudyTest {
     }
 
     @Test
-    @DisplayName("스터디 만들기2 😀")
+    @DisplayName("스터디 만들기 assertThrows😀")
     void create_new_study2() {
 
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
@@ -44,7 +54,7 @@ class StudyTest {
     }
 
     @Test
-    @DisplayName("스터디 만들기3 😀")
+    @DisplayName("스터디 만들기 assertTimeout😀")
     void create_new_study3() {
 
         assertTimeout(Duration.ofMillis(100), () -> {
@@ -59,6 +69,85 @@ class StudyTest {
         });
         // assertTimeoutPreemptively의 경우, 100밀리세컨이 지나면 바로 테스트를 실패하게 한다.
 
+    }
+
+    @DisplayName("스터디 만들기 @RepeatedTest😀")
+    @RepeatedTest(value = 10, name = "{currentRepetition}/{totalRepetitions} {displayName}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+
+        System.out.println("test " + repetitionInfo.getCurrentRepetition() + "/" +
+                repetitionInfo.getTotalRepetitions());
+
+    }
+
+    @DisplayName("스터디 만들기 @ParameterizedTest 문자열😀")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "더워지고", "있네요."})
+
+//    @EmptySource
+//    @NullSource
+    @NullAndEmptySource
+    void parameterizedTest(String message) {
+        System.out.println(message);
+    }
+
+    @DisplayName("스터디 만들기 @ParameterizedTest 숫자열😀")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {10, 20, 40})
+    void parameterizedTest2(Integer limit) {
+        System.out.println(limit);
+    }
+
+    // Study 형태로 받을 수도 있다.
+    @DisplayName("스터디 만들기 ArgumentConverter사용😀")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {10, 20, 40})
+    void parameterizedTest2(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println(study.getLimit());
+    }
+
+    // ArgumentConverter는 인자가 1개일때만 사용 가능하다.
+    static class StudyConverter extends SimpleArgumentConverter {
+
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    // 2개의 인자 넘기기
+    @DisplayName("스터디 만들기 복수 인자 넘기기😀")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTest3(Integer limit, String name) {
+        Study study = new Study(limit, name);
+        System.out.println(study);
+    }
+
+    // 혹은 Study로 받는 방법 (ArgumentsAccessor)
+    @DisplayName("스터디 만들기 복수 인자 넘기기 ArgumentsAccessor사용😀")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTest4(ArgumentsAccessor argumentsAccessor) {
+        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        System.out.println(study);
+    }
+
+    // 혹은 Study로 받는 방법 (custom Aggregator생성)
+    @DisplayName("스터디 만들기 복수 인자 넘기기 ArgumentsAggregator사용😀")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTest5(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    // ArgumentsAggregator 인자가 여러개 일 때 사용.
+    static class StudyAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
     }
 
     @Test
